edbcdf3ca11a72940d0f2ccaa6abd68a527bab86
gyro <aura.uyt@gmail.com>
2026-01-10 03:02:58 +0530

    Added fix for tree-sitter error

--- src/App.jsx
+++ src/App.jsx
@@ -258,25 +258,57 @@ const SETUP_LAYERS = [
     description: "Dotfiles and specific personal tweaks",
     items: [
       {
-        id: "dotfiles",
-        name: "Dotfiles Repo",
-        desc: "Clone my config",
-        cmd: "git clone https://github.com/gyro-uyt/dotfiles.git ~/.dotfiles",
-        tags: ["Config"],
-      },
-      {
-        id: "ssh-keys",
-        name: "SSH Keys",
-        desc: "Generate GitHub keys",
-        cmd: 'ssh-keygen -t ed25519 -C "your-email@example.com"',
-        tags: ["Security"],
-      },
-      {
-        id: "git-config",
-        name: "Git Identity",
-        desc: "Set global user",
-        cmd: 'git config --global user.name "Your Name" && git config --global user.email "you@example.com"',
-        tags: ["Config"],
+        id: "neovim",
+        name: "Neovim",
+        desc: "Configuring it up...",
+        cmd: `pacman -S neovim`,
+        tags: ["IDE"],
+        subCommands: [
+          {
+            label:
+              "Fix for tree-sitter error (KickStart Nvim): Replace the code block 941:962 in init.lua",
+            code: `
+{ -- Highlight, edit, and navigate code
+"nvim-treesitter/nvim-treesitter",
+build = ":TSUpdate",
+lazy = true, -- just here
+main = "nvim-treesitter.configs", -- Sets main module to use for opts
+-- [[ Configure Treesitter ]] See :help nvim-treesitter
+opts = {
+ensure_installed = {
+"bash",
+"c",
+"diff",
+"html",
+"lua",
+"luadoc",
+"markdown",
+"markdown_inline",
+"query",
+"vim",
+"vimdoc",
+},
+-- Autoinstall languages that are not installed
+auto_install = true,
+highlight = {
+enable = true,
+-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+-- If you are experiencing weird indenting issues, add the language to
+-- the list of additional_vim_regex_highlighting and disabled languages for indent.
+additional_vim_regex_highlighting = { "ruby" },
+},
+indent = { enable = true, disable = { "ruby" } },
+},
+-- There are additional nvim-treesitter modules that you can use to interact
+-- with nvim-treesitter. You should go explore a few and see what interests you:
+--
+-- - Incremental selection: Included, see :help nvim-treesitter-incremental-selection-mod
+-- - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+-- - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+},
+            `,
+          },
+        ],
       },
     ],
   },
